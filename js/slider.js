afcac35716f88b0c0cd99a4d684d50eee64c0ad4
Urszula Dziewit <urszula.dziewit@inwenta.pl>
2017-06-25 11:48:26 +0200

    final

---
+++ js/slider.js
@@ -0,0 +1,83 @@
+'use strict';
+$(function () {
+
+	//zmienne
+
+	var slideCount = $('.single-slide').length;
+	var slideShow = $('.slide-show');
+	var slideWidth = 100 / slideCount;
+	var slideIndex = 0;
+
+	/*funkcja slide*/
+	function slide(newSlideIndex) {
+		console.log('TEST');
+		if(newSlideIndex < 0 || newSlideIndex >= slideCount) {
+			return;
+		}
+	
+		var marginLeft = (newSlideIndex * (-100)) + '%';
+
+		slideShow.animate({
+			'margin-left': marginLeft
+		}, 800, function(){
+			slideIndex = newSlideIndex;
+		});
+	}
+	
+
+/*szerokość kontenera*/
+
+slideShow.css('width', slideCount * 100 + '%'); console.log(slideCount);
+
+
+
+	/*szerokość i położenie pojedynczego slajdu*/
+
+	$('.single-slide').each(function (index) {
+
+		$(this).css({
+			'width': slideWidth + '%',
+			'margin-left': index * slideWidth + '%'
+		});
+	});
+
+	/* klikniecie strzalki*/
+
+	$('.prev-slide').click(function () {
+		slide(slideIndex - 1);
+	});
+
+
+	$('.next-slide').click(function () {
+		slide(slideIndex + 1);
+
+		/*if(newSlideIndex >= slideCount) {
+			return;
+		}*/
+
+
+
+
+	});
+/*var {newSlideIndex = slideIndex - 1;
+		
+if(newSlideIndex <0) {
+	return;
+}*/
+
+
+/*var marginLeft = (newSlideIndex * (-100)) + '%';
+slideShow.animate({'marginLeft': marginLeft}, 800, function(){
+	slideIndex = newSlideIndex;
+});*/
+});
+
+
+
+
+
+
+
+
+/*single-slide.css('width', 100/slideCount + '%');
+console.log(slideCount);*/
